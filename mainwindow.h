f6491582d1647b6ad3a89f9e09eefffe749603cf
M00TP <eisflamme@mihahome.de>
2017-04-03 09:55:50 +0200

    added LGPL headers

--- mainwindow.h
+++ mainwindow.h
@@ -1,3 +1,23 @@
+/*
+    Elypson/qt-collapsible-section
+    (c) 2016 Michael A. Voelkel - michael.alexander.voelkel@gmail.com
+
+    This file is part of Elypson/qt-collapsible section.
+
+    Elypson/qt-collapsible-section is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Elypson/qt-collapsible-section is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Elypson/qt-collapsible-section. If not, see <http://www.gnu.org/licenses/>.
+*/    
+
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
