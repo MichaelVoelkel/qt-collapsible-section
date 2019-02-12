fbca134ba1a9d1362422d2744e6a17aefcb4f1e8
IceMichael <eisflamme@mihahome.de>
2019-02-12 08:57:01 +0100

    Update Section.cpp
    clicked signal replaced by toggled signal to also react to `setChecked`

--- Section.cpp
+++ Section.cpp
@@ -62,7 +62,7 @@ Section::Section(const QString & title, const int animationDuration, QWidget* pa
     mainLayout->addWidget(contentArea, row, 0, 1, 3);
     setLayout(mainLayout);
 
-    QObject::connect(toggleButton, &QToolButton::clicked, [this](const bool checked)
+    QObject::connect(toggleButton, &QToolButton::toggled, [this](const bool checked)
     {
         toggleButton->setArrowType(checked ? Qt::ArrowType::DownArrow : Qt::ArrowType::RightArrow);
         toggleAnimation->setDirection(checked ? QAbstractAnimation::Forward : QAbstractAnimation::Backward);
